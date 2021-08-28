9b5c6d7954fb929b4bf7400c4148f45270de1501
mattj241 <londonm241@gmail.com>
2021-08-28 02:29:15 -0400

    Improving Relational Model Design more

--- capstone/starter/app.py
+++ capstone/starter/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import Vehicle, setup_db, session_revert, session_close
+from models import *
 
 def create_app(test_config=None):
   # create and configure the app
@@ -20,34 +20,118 @@ def create_app(test_config=None):
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-  @app.route('/vehicles', methods=['GET'])
+  @app.route('/vehicles/',  methods=['GET'])
   def get_vehicles():
     vehicles = Vehicle.query.all()
     dict = {}
     for vehicle in vehicles:
       dict.update(vehicle.format())
     return dict
+
+  @app.route('/vehicles/<int:id>', methods=['GET'])
+  def get_vehicle(id):
+      vehicle = Vehicle.query.filter_by(id=id).first()
+
+      return vehicle.format()
   
   @app.route('/vehicles', methods=['POST'])
   def add_vehicle():
     data = request.get_json()
+    try:
+      new_vehicle = Vehicle(data['make'],
+                            data['model'],
+                            data['year'],
+                            data['body_style'],
+                            data['color'],
+                            data['currently_rented'])
+      Vehicle.insert(new_vehicle)
+    except Exception:
+      session_revert()
+      abort(422)
+    finally:
+      session_close()
+    return jsonify({
+        "message" : "201 Vehicle added" 
+      })
+
+  @app.route('/customers', methods=['GET'])
+  def get_customers():
+    customers = Customer.query.all()
+    dict = {}
+    for customer in customers:
+      dict.update(customer.format())
+    return dict
+  
+  @app.route('/customers', methods=['POST'])
+  def add_customers():
+    data = request.get_json()
     # try:
-    new_vehicle = Vehicle(data['make'],
-                          data['model'],
-                          data['year'],
-                          data['body_style'],
-                          data['color'],
-                          data['currently_rented'],
-                          data['renter_id'],
-                          data['home_branch_id'])
-    Vehicle.insert(new_vehicle)
+    new_cust = Customer(data['first_name'],
+                          data['last_name'],
+                          data['address'],
+                          data['type'],
+                          data['currently_renting'])
+    Customer.insert(new_cust)
     # except Exception:
     #   session_revert()
     #   abort(422)
     # finally:
     #   session_close()
     return jsonify({
-        "message" : "201 Question successfully created" 
+        "message" : "201 Customer added" 
+      })
+
+  @app.route('/managers', methods=['GET'])
+  def get_managers():
+    managers = Manager.query.all()
+    dict = {}
+    for manager in managers:
+      dict.update(manager.format())
+    return dict
+  
+  @app.route('/managers', methods=['POST'])
+  def add_managers():
+    data = request.get_json()
+    # try:
+    new_manager = Manager(data['first_name'],
+                          data['last_name'],
+                          data['address'],
+                          data['type'])
+    Manager.insert(new_manager)
+    # except Exception:
+    #   session_revert()
+    #   abort(422)
+    # finally:
+    #   session_close()
+    return jsonify({
+        "message" : "201 Manager added" 
+      })
+
+  @app.route('/employees', methods=['GET'])
+  def get_employees():
+    employees = Employee.query.all()
+    dict = {}
+    for employee in employees:
+      dict.update(employee.format())
+    return dict
+  
+  @app.route('/employees', methods=['POST'])
+  def add_employees():
+    data = request.get_json()
+    try:
+      new_cust = Employee(data['first_name'],
+                            data['last_name'],
+                            data['address'],
+                            data['type'],
+                            data['manager_id'])
+      Employee.insert(new_cust)
+    except Exception:
+      session_revert()
+      abort(422)
+    finally:
+      session_close()
+    return jsonify({
+        "message" : "201 Employee added" 
       })
     
   
--- capstone/starter/models.py
+++ capstone/starter/models.py
@@ -6,10 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.sql.expression import false
+from sqlalchemy.sql.schema import ForeignKey, PrimaryKeyConstraint, Table, MetaData
+from sqlalchemy.sql.sqltypes import Boolean, Float
 from config import init_env_vars
+Base = declarative_base()
+
 
 init_env_vars()
 database_name = "car_rental"
@@ -42,8 +45,18 @@ def session_close():
 Schema Configuration
 
 '''
-
-Base = declarative_base()
+class Reservation (db.Model):
+    __tablename__ = 'reservation'
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'),   primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customer.id'), primary_key=True)
+    employee_id = Column(Integer, ForeignKey('employee.id'), primary_key=True)
+    # implementing time always sucks, will come back if enough time
+    # start_time = 
+    # end_time = 
+    cost = Column(Float, nullable=False)
+    vehicle  = relationship('Vehicle', uselist=False, foreign_keys=[vehicle_id])
+    customer = relationship('Customer', uselist=False, foreign_keys=[customer_id])
+    employee = relationship('Employee', uselist=False, foreign_keys=[employee_id])
 
 class Vehicle(db.Model):
     __tablename__= 'vehicle'
@@ -55,19 +68,16 @@ class Vehicle(db.Model):
     body_style = Column(String)
     color = Column(String)
     currently_rented = Column(Boolean, nullable=False)
-    renter_id = relationship('Customer', backref='vehicles', uselist=False)
-    home_branch_id = relationship('Branch', backref='vehicles', uselist=False)
+    reservations = relationship('Reservation', back_populates='vehicle')
 
     def __init__(self, make, model, year, body_style, color,
-                currently_rented, renter_id, home_branch_id):
+                currently_rented):
         self.make = make
         self.model = model
         self.year = year
         self.body_style = body_style
         self.color = color
         self.currently_rented = currently_rented
-        self.renter_id = renter_id
-        self.home_branch_id = home_branch_id
 
     def insert(self):
         db.session.add(self)
@@ -82,97 +92,143 @@ class Vehicle(db.Model):
     
     def format(self):
         return {
+            'id' : self.id,
             'make': self.make,
             'model': self.model,
             'year': self.year,
             'body_style': self.body_style,
             'color': self.color,
             'currently_rented': self.currently_rented,
-            'renter_id': self.renter_id,
-            'home_branch_id': self.home_branch_id
         }
 
 class Person(db.Model):
+    # __tablename__= 'person'
     __abstract__ = True
 
+    # id = Column(Integer, primary_key=True)
     first_name = Column(String, nullable=False)
     last_name  = Column(String, nullable=False)
     address = Column(String, nullable=False)
-    currently_renting = Column(Boolean, nullable=False)
+    type = Column(String(50))
 
-    # Credit: https://learnings.desipenguin.com/post/fk_abstract_class_sqlalchemy/
-    @declared_attr
-    def vehicle_id(cls):
-        rented_vehicle = Column(Integer, ForeignKey('vehicle.id'))
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'person',
+    }
 
-    def __init__(self, first_name, last_name, address, currently_renting, body_style, color,
-                rented_vehicle):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.currently_renting = currently_renting
-        self.rented_vehicle = rented_vehicle
 
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-    
-    def format(self):
-        return {
-            'first_name' : self.first_name,
-            'last_name' : self.last_name,
-            'address' : self.address,
-            'currently_renting' : self.currently_renting,
-            'rented_vehicle' : self.rented_vehicle,
-        }
-
-class Branch(db.Model):
-    __tablename__ = 'branch'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    address = Column(String, nullable=False)
-    # staff = relationship('Staff', backref='branch')
-    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
-
-    def __init__(self, name, address, staff):
-        self.name = name
-        self.address = address
-        self.staff = staff
-
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-    
-    def format(self):
-        return {
-            'name' : self.name,
-            'address' : self.address,
-            'staff' : self.staff
-        }
 class Customer(Person):
     __tablename__ = 'customer'
 
     id = Column(Integer, primary_key=True)
-    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    currently_renting = Column(Boolean, nullable=False)
+    reservations = relationship('Reservation', back_populates='customer')
+
+    __mapper_args__ = {
+        'polymorphic_identity':'customer'
+    }
+
+    def __init__(self, first_name, last_name, address, type, currently_renting):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.type = type
+        self.currently_renting = currently_renting
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id' : self.id,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'address' : self.address,
+            'type' : self.type,
+            'currently_renting' : self.currently_renting
+        }
 
 
-class Staff(Person):
-    __tablename__ = 'staff'
+class Manager(Person):
+    __tablename__ = 'manager'
 
     id = Column(Integer, primary_key=True)
-    branch = Column(Integer, ForeignKey('branch.id'))
+    employees = relationship('Employee', back_populates='manager')
+
+    __mapper_args__ = {
+        'polymorphic_identity':'manager'
+    }
+
+    def __init__(self, first_name, last_name, address, type):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.type = type
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id' : self.id,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'address' : self.address,
+            'type' : self.type
+        }
+
+class Employee(Person, db.Model):
+    __tablename__ = 'employee'
+
+    id = Column(Integer, primary_key=True)
+    manager_id = Column(Integer, ForeignKey('manager.id'))
+    manager = relationship('Manager', back_populates='employees')
+    reservations = relationship('Reservation', back_populates='employee')
+
+    __mapper_args__ = {
+        'polymorphic_identity':'employee'
+    }
+
+    def __init__(self, first_name, last_name, address, type, manager_id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.type = type
+        self.manager_id = manager_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id' : self.id,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'address' : self.address,
+            'type' : self.type,
+            'manager_id' : self.manager_id
+        }
